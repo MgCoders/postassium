16f099319c1e1395c177697cfa29deea0c9fa6d3
Pablo Olivera <polivera@mgcoders.com>
2018-06-07 22:52:45 -0300

    notificaciones destinatarios - configuraciones

--- src/main/java/coop/magnesium/potassium/db/entities/Role.java
+++ src/main/java/coop/magnesium/potassium/db/entities/Role.java
@@ -4,5 +4,5 @@ package coop.magnesium.potassium.db.entities;
  * Created by rsperoni on 31/05/17.
  */
 public enum Role {
-    USER, ADMIN
+    USER, ADMIN, SUPER_ADMIN
 }
---
+++ src/main/java/coop/magnesium/potassium/db/entities/TipoConfiguracion.java
@@ -0,0 +1,20 @@
+package coop.magnesium.potassium.db.entities;
+
+/**
+ * Created by rsperoni on 19/01/18.
+ */
+public enum TipoConfiguracion {
+    NOTIFICACION_ADMIN_DESTINATARIO,
+    NOTIFICACION_PERIODICIDAD,
+    NOTIFICACION_MAIL_ACTIVADO,
+    NODO_MASTER,
+    MAIL_FROM,
+    MAIL_PASS,
+    MAIL_HOST,
+    MAIL_PORT,
+    PROJECT_NAME,
+    LOGO_URL,
+    FRONTEND_HOST,
+    FRONTEND_PATH,
+    REST_BASE_PATH
+}
